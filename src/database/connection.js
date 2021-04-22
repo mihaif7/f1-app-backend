f19bc2e6c34e4639220bf624ca737b8e03e90ef9
Mihai <mihai.fizedean@gmail.com>
2021-04-22 22:37:34 +0300

    added cloud db

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,14 +1,17 @@
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
-  "f1_app",
-  "root",
-  "123456",
+  "heroku_2b84991d910ee92",
+  "bf4777616fe61d",
+  "63ff8370",
   {
-    host: "localhost",
+    host: "eu-cdbr-west-01.cleardb.com",
     dialect: "mysql"
   }
 );
 
 module.exports = sequelize;
 global.sequelize = sequelize;
+
+
+// mysql://bf4777616fe61d:63ff8370@eu-cdbr-west-01.cleardb.com/heroku_2b84991d910ee92?reconnect=true
