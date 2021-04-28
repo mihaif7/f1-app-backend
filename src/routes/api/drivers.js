701ac749a20573517ff3f2a0d7f8dfcfb57e6abf
Mihai <mihai.fizedean@gmail.com>
2021-04-28 19:30:56 +0300

    added color for head to head

--- src/routes/api/drivers.js
+++ src/routes/api/drivers.js
@@ -7,9 +7,23 @@ const { QueryTypes } = require("sequelize");
 router.get("/:raceId", async (req, res) => {
   const results = await sequelize
     .query(
-      `SELECT d.driverId, d.number, code, forename, surname FROM drivers as d
-        INNER JOIN results as r ON d.driverId = r.driverId WHERE r.raceId =${req.params.raceId}
-        ORDER BY number`,
+      `SELECT 
+          d.driverId,
+          d.number,
+          code,
+          forename,
+          surname,
+          c.constructorRef
+       FROM
+          drivers AS d
+              INNER JOIN
+          results AS r ON d.driverId = r.driverId
+              INNER JOIN
+          constructors AS c ON c.constructorId = r.constructorId
+              AND r.driverId = d.driverId
+       WHERE
+          r.raceId = ${req.params.raceId}
+       ORDER BY number`,
       {
         type: QueryTypes.SELECT,
       }
